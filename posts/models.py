49b2409e06e1ee3a55a0d7a16c6f568f7259b056
ALi <samadi_as@yahoo.com>
2020-11-20 14:33:12 +0330

    __string__

--- posts/models.py
+++ posts/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Post (models.Model):
-    text = models.TextField();
+    text = models.TextField()
+
+    def __string__(self):
+        return self.text[:50]
